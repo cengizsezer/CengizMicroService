40d2723a052711a0cfca23912862d4e4306c73fe
Cengiz <cengizsezer7@gmail.com>
2025-07-26 17:43:25 +0300

    change password

--- src/AppGateways/WebAppGateway/Web.ApiGateway/obj/Debug/net8.0/Web.ApiGateway.AssemblyInfo.cs
+++ src/AppGateways/WebAppGateway/Web.ApiGateway/obj/Debug/net8.0/Web.ApiGateway.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web.ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+502625404bdededfd519fd791925e67c1cfde0c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f944bdcd7ae5772087d3effef9903907270513e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web.ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web.ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
