686af32e6bd8a5439916d606b72243731d24d135
Cengiz <cengizsezer7@gmail.com>
2025-07-30 18:24:47 +0300

    go

--- src/AppGateways/WebAppGateway/Web.ApiGateway/obj/Debug/net8.0/Web.ApiGateway.AssemblyInfo.cs
+++ src/AppGateways/WebAppGateway/Web.ApiGateway/obj/Debug/net8.0/Web.ApiGateway.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web.ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+728c3c8c2daa9f2b036b7b23f9b5d9cdfb2deaff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bda58dd7b5816fca3f97de147ccec52aa4b133a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web.ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web.ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
