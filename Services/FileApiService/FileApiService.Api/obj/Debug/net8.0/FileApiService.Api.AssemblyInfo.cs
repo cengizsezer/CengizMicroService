a3601bab686b027aacecfc057c6eba16ae578535
Cengiz <cengizsezer7@gmail.com>
2025-11-03 23:05:57 +0300

    go

--- Services/FileApiService/FileApiService.Api/obj/Debug/net8.0/FileApiService.Api.AssemblyInfo.cs
+++ Services/FileApiService/FileApiService.Api/obj/Debug/net8.0/FileApiService.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileApiService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4af898f31b301f96e05cdcf7b5a1f4d90a200bc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+466228e11239b2d722799fc9824d239159f75ca9")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileApiService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileApiService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Services/NotificationService/NotificationService.Console/obj/Debug/net8.0/NotificationService.Console.AssemblyInfo.cs
+++ Services/NotificationService/NotificationService.Console/obj/Debug/net8.0/NotificationService.Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotificationService.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4af898f31b301f96e05cdcf7b5a1f4d90a200bc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+466228e11239b2d722799fc9824d239159f75ca9")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotificationService.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotificationService.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
