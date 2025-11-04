320cf44336dba2b85c184dd5db4a58e3047d1698
Cengiz <cengizsezer7@gmail.com>
2025-11-04 13:00:02 +0300

    go

--- Services/FileApiService/FileApiService.Api/obj/Debug/net8.0/FileApiService.Api.AssemblyInfo.cs
+++ Services/FileApiService/FileApiService.Api/obj/Debug/net8.0/FileApiService.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileApiService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3601bab686b027aacecfc057c6eba16ae578535")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af4664f8947e669ff2435abe220318a84c074510")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileApiService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileApiService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/AppGateways/WebAppGateway/Web.ApiGateway/obj/Debug/net8.0/Web.ApiGateway.AssemblyInfo.cs
+++ src/AppGateways/WebAppGateway/Web.ApiGateway/obj/Debug/net8.0/Web.ApiGateway.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web.ApiGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3601bab686b027aacecfc057c6eba16ae578535")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af4664f8947e669ff2435abe220318a84c074510")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web.ApiGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web.ApiGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
